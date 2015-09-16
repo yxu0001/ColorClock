f2f043f81910fb2a5f7b06637dafdf4aea44b176
yxu0001@yahoo.com <yxu0001@yahoo.com>
2015-09-16 15:18:49 -0700

    initial proj

---
+++ ColorClock/ColorClock/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  ColorClock
+//
+//  Created by Yijia Xu on 9/16/15.
+//  Copyright © 2015 athenahealth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var clockLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
