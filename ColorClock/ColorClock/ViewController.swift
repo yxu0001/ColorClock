1dae9f2f3d9ae05600199f7bc8209ebfb140f290
yxu0001@yahoo.com <yxu0001@yahoo.com>
2015-09-16 15:35:05 -0700

    timer

--- ColorClock/ColorClock/ViewController.swift
+++ ColorClock/ColorClock/ViewController.swift
@@ -11,18 +11,27 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var timeLabel: UILabel!
-    let date = NSDate()
     let dateFormatter = NSDateFormatter()
+    var timer: NSTimer?
+    
+    //= NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: "updateTimer:", userInfo: nil, repeats: true)
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        timer = NSTimer.scheduledTimerWithTimeInterval(1,
+                                target: self,
+                                selector: "updateTimer:",
+                                userInfo: nil,
+                                repeats: true)
+        
         dateFormatter.locale = NSLocale(localeIdentifier: "en_GB")
         dateFormatter.dateStyle = .NoStyle
         dateFormatter.timeStyle = .MediumStyle
         
+        let date = NSDate()
         self.timeLabel.text = dateFormatter.stringFromDate(date)
     }
 
@@ -31,6 +40,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func updateTimer(timer: NSTimer)
+    {
+        let date = NSDate()
+        self.timeLabel.text = dateFormatter.stringFromDate(date)
+    }
+    
 }
 
